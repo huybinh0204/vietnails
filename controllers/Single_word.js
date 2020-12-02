75e0743e26daf82e5b2a1768596e6021b9a08da4
huybinh0204 <huybinh020403@gmail.com>
2020-12-02 13:49:40 +0700

    update vvvv

--- controllers/Single_word.js
+++ controllers/Single_word.js
@@ -65,7 +65,7 @@ module.exports = {
             }
             var _Arrsingle_word = JSON.stringify(obj);
             var single_wordJson = JSON.parse(_Arrsingle_word);
-            var ArrGetsingle_word = [{"status": "200","message": 'Single_word list !', "data": single_wordJson}]
+            var ArrGetsingle_word = [{"status": "200", "message": 'Single_word list !', "data": single_wordJson}]
             res.json(ArrGetsingle_word);
         })
     },
@@ -90,40 +90,48 @@ module.exports = {
             }
             var _Arrsingle_word = JSON.stringify(obj);
             var single_wordJson = JSON.parse(_Arrsingle_word);
-            var ArrGetsingle_word = [{"status": "200","message": 'Single_word detail !', "data": single_wordJson}]
+            var ArrGetsingle_word = [{"status": "200", "message": 'Single_word detail !', "data": single_wordJson}]
             res.json(ArrGetsingle_word);
         })
     },
     list_mployees: (req, res) => {
-        let begin_date = req.body.begin_date;
-        let sql = `SELECT * FROM single_word WHERE single_word.end_date >= "${begin_date}%" and single_word.begin_date <= "${begin_date}%"`
+        var begin_date = req.body.begin_date;
+        let sql =``;
+        if(begin_date == undefined || begin_date == ""){
+            var a = new Date();
+            begin_date_ = a.getFullYear()+"-"+(a.getMonth()+1)+"-"+(a.getDay()-1);
+            sql = `SELECT * FROM user WHERE id NOT IN (SELECT id_User FROM single_word WHERE ` +
+                `single_word.end_date >= "${begin_date_}%" and single_word.begin_date <= "${begin_date_}%") and id_roles = 2 and is_active =0`;
+        }else {
+            sql = `SELECT * FROM user WHERE id NOT IN (SELECT id_User FROM single_word WHERE ` +
+                `single_word.end_date >= "${begin_date}%" and single_word.begin_date <= "${begin_date}%") and id_roles = 2 and is_active =0`;
+        }
         db.query(sql, [req.params.single_wordId], (err, rown, fields) => {
                 if (err) throw err
-                let sqlk = `SELECT * FROM user WHERE id_roles = 2 and is_active =0`;
-                db.query(sqlk, (err, rownn, fields) => {
-                    if (err) throw err
-                    var obj = [];
-                    for (var i = 0; i < rownn.length; i++) {
-                        var ArrUser = {
-                            [user_model.id]: rownn[i].id,
-                            [user_model.phone]: rownn[i].phone,
-                            [user_model.email]: rownn[i].email,
-                            [user_model.fullName]: rownn[i].fullName,
-                            [user_model.id_roles]: rownn[i].id_roles,
-                            [user_model.avatar]: rownn[i].avatar,
-                            [user_model.address]: rownn[i].address,
-                            [user_model.birthday]: rownn[i].birthday,
-                            [user_model.gender]: rownn[i].gender,
-                            [user_model.is_active]: rownn[i].is_active,
-                            [user_model.created_user]: rownn[i].created_user
-                        };
-                        obj.push(ArrUser);
-                    }
-                    var _ArrUser = JSON.stringify(obj);
-                    var UserJson = JSON.parse(_ArrUser);
-                    var ArrGetUser = [{"status": "200","message": 'User mployees go to work!', "data": UserJson}]
-                    res.json( ArrGetUser)
-                })
+
+                //     let difference = a.filter(x => !b.includes(x));
+                var obj = [];
+                for (var i = 0; i < rown.length; i++) {
+                    var ArrUser = {
+                        [user_model.id]: rown[i].id,
+                        [user_model.phone]: rown[i].phone,
+                        [user_model.email]: rown[i].email,
+                        [user_model.fullName]: rown[i].fullName,
+                        [user_model.id_roles]: rown[i].id_roles,
+                        [user_model.avatar]: rown[i].avatar,
+                        [user_model.address]: rown[i].address,
+                        [user_model.birthday]: rown[i].birthday,
+                        [user_model.gender]: rown[i].gender,
+                        [user_model.is_active]: rown[i].is_active,
+                        [user_model.created_user]: rown[i].created_user
+                    };
+                    obj.push(ArrUser);
+                }
+                var _ArrUser = JSON.stringify(obj);
+                var UserJson = JSON.parse(_ArrUser);
+                var ArrGetUser = [{"status": "200", "message": 'User mployees go to work!', "data": UserJson}]
+                res.json(ArrGetUser);
+
             }
         )
     },
