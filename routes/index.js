6965c1ca03a23d33fbccacab831ea3c74e8774ff
huybinh0204 <huybinh020403@gmail.com>
2020-12-21 08:43:58 +0700

    updat notify

--- routes/index.js
+++ routes/index.js
@@ -229,7 +229,7 @@ module.exports = function (app) {
     //     .get(Notify_UserCtrl.get_notify_nv);
 
 
-    cron.schedule('*/15 * * * *', () => {
+    cron.schedule('*/1 * * * *', () => {
         app.get(Notify_UserCtrl.get_time_schedule())
         app.get(Notify_UserCtrl.get_notify_nv())
         app.get(Notify_UserCtrl.get_notify_kh())
