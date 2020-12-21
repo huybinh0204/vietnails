a8a1b657b3f8ae676379f898ff0bd6a9b3a67374
huybinh0204 <huybinh020403@gmail.com>
2020-12-22 02:22:42 +0700

    updat nhuy don

--- controllers/Notification.js
+++ controllers/Notification.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
+const db = require('../service');
+module.exports = {
+    detail: (req, res) => {
+        let sql = 'SELECT * FROM notification WHERE id_User = ?'
+        db.query(sql, [req.params.idUserNotification], (err, rown, fields) => {
+            if (err) throw err
+            var obj = [];
+            for (var i = 0; i < rown.length; i++) {
+                var Arrnotification = {
+                    id: rown[i].id,
+                    id_User: rown[i].id_User,
+                    receiver_name: rown[i].receiver,
+                    date_notification: rown[i].date_notification,
+                };
+                obj.push(Arrnotification);
+            }
+            var _Arrnotification = JSON.stringify(obj);
+            var notificationJson = JSON.parse(_Arrnotification);
+            var ArrGetnotification = [{"status": "200", "data": notificationJson}]
+            res.json(ArrGetnotification);
+        })
+    },
+}
